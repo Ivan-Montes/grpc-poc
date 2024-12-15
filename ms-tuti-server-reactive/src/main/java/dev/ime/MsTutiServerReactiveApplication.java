2927a5d60236fe87930c7d3401987175bad9c041
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-12-15 10:26:42 +0100

    feat: initial commit grpc server reactive

---
+++ ms-tuti-server-reactive/src/main/java/dev/ime/MsTutiServerReactiveApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTutiServerReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTutiServerReactiveApplication.class, args);
+	}
+
+}
