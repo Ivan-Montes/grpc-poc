4bd9c97f314d6322394c1df0f225cc860e26be50
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-12-17 07:28:36 +0100

    feat: initial commit server-and-client microservice

---
+++ ms-tuti-client-reactive/src/main/java/dev/ime/MsTutiClientReactiveApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTutiClientReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTutiClientReactiveApplication.class, args);
+	}
+
+}
